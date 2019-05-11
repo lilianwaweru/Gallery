1e9d17785dbe233009f51a389105843d867510c4
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-11 15:33:48 +0300

    created image class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    # image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length = 30)
+    description = models.TextField()
+
+
--- photos/settings.py
+++ photos/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'photos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos',
+        'USER': 'lilian',
+        'PASSWORD':'12345',
     }
 }
 
