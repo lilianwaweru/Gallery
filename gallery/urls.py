b051f30a4983fb7db9cac7b969658137215df2f9
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-13 03:56:14 +0300

    added a search button

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search_category, name='search_category')
+    #  url(r'^location/(\d+)',views.display_location,name='displayLocation'),
+
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+def search_category(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = (request.GET.get("category")).title()
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request, 'search.html',{"message":message})    
