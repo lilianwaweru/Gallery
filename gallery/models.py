2a749b6b209c339708c0372b11dce3ae17992a1c
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-14 08:49:59 +0300

    added location.html

--- gallery/models.py
+++ gallery/models.py
@@ -43,6 +43,6 @@ class Image(models.Model):
 
     @classmethod
     def search_by_category(cls,search_term):
-        images = cls.objects.filter(image_category__name__contains = search_term)
+        images = cls.objects.filter(category__name__icontains = search_term)
         return images
 
--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Image,Category,Location
 
 # Create your views here.
 def welcome(request):
     images = Image.objects.all()
-    return render(request, 'welcome.html',{'images':images})
+    return render(request,'welcome.html',{'images':images})
 
 
 def search_category(request):
